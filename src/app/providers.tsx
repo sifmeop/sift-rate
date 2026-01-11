68a8e923506e503b7db50848f9ec092a5425ae81
Eugene <sifmeop@gmail.com>
2026-01-11 12:07:49 +0200

    fix: add custom toastProps to ToastProvider for improved styling

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -9,7 +9,14 @@ export const Providers = ({ children }: React.PropsWithChildren) => {
     <HeroUIProvider>
       <ThemeProvider attribute='class' defaultTheme='dark'>
         <LazyMotion features={domAnimation}>
-          <ToastProvider placement='top-center' />
+          <ToastProvider
+            placement='top-center'
+            toastProps={{
+              classNames: {
+                base: 'top-[calc(65px+env(safe-area-inset-top))] md:top-20'
+              }
+            }}
+          />
           <Layout>{children}</Layout>
         </LazyMotion>
       </ThemeProvider>
