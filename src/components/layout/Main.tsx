2d9286d95f65245368c196616be3b57ad3aaeeeb
Eugene <sifmeop@gmail.com>
2026-01-11 11:55:47 +0200

    fix: adjust padding values in Main component for improved layout

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,6 +1,6 @@
 export const Main = ({ children }: React.PropsWithChildren) => {
   return (
-    <main className='app-container z-px pt-[calc(65px+16px+env(safe-area-inset-top))] pb-[calc(69px+16px+env(safe-area-inset-bottom))]'>
+    <main className='app-container z-px pt-[calc(65px+10px+env(safe-area-inset-top))] pb-[calc(69px+16px+env(safe-area-inset-bottom))] md:pt-25 md:pb-5'>
       {children}
     </main>
   )
