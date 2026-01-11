13fcbc778223879f8b65adc561919032466b42a1
Eugene <sifmeop@gmail.com>
2026-01-11 10:57:10 +0200

    fix: update padding values in Main and MobileNav components for improved layout

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,6 +1,6 @@
 export const Main = ({ children }: React.PropsWithChildren) => {
   return (
-    <main className='app-container z-px pt-safe-or-22 pb-safe-or-24 md:pb-safe-or-4 relative'>
+    <main className='app-container z-px pt-safe-or-30 pb-safe-or-24 md:pb-safe-or-4 relative'>
       {children}
     </main>
   )
